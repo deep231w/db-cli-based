f1868a08aa17b6a7473e2f1350ec5b19aeac13ad
Deepak Maharana <100968930+deep231w@users.noreply.github.com>
2026-01-30 01:46:38 +0530

    add user to db and fetch users from db functionality is complete

--- db.c
+++ db.c
@@ -1,24 +1,47 @@
 #include<stdio.h>
+#include<stdlib.h>
 #include<string.h>
-#define MAX_INPUT 1024
 
+
+#define MAX_INPUT 1024
+#define MAX_USERS 100
 
 typedef struct 
 {
     int id;
     char name[20];
     int age;
-}user;
+}User;
 
+User *db[MAX_USERS];
+int db_users=0;
 
-void addUserTodb(char *id , char *name , char *age){
+void addUserTodb(int lid , char *lname , int lage){
+    User *ptr= (User *)malloc(sizeof(User));
+    if(ptr ==NULL){
+    	printf("memory allocation failed!\n");
+	return;
+    }  
 
+    ptr->id=lid;
+    //for(int i=0; i<sizeof(lname);i++){
+    //	ptr->name[i]=lname[i];
+    //}
+    strcpy(ptr->name,lname);
+    ptr->age=lage;
     
-    printf("user data inside addusertodb func- %s %s %s", id , name , age);
+    db[db_users++]=ptr;
+    printf("user data inside addusertodb func- %d %s %d", lid , lname , lage);
 
     printf("user added to db successfully!\n");
 }
 
+void ListAllUsers(){
+	printf("inside list all users func.\n");
+	for(int i=0; i<db_users; i++){
+		printf("%d %s %d\n", db[i]->id ,db[i]->name, db[i]->age);
+	}
+}
 int main(){
     char input[MAX_INPUT];
 
@@ -32,7 +55,7 @@ int main(){
             break;
         }
 
-        char *token = strtok(input , " ");
+        char *token = strtok(input , " \n");
         printf("token- %s\n", token);
 
         // while (token != NULL)
@@ -48,13 +71,25 @@ int main(){
         }
 
         if(strcmp(token, "add")==0){
-            char *id = strtok(NULL , " ");
+            char *id_str = strtok(NULL , " ");
             char *name= strtok(NULL , " ");
-            char *age= strtok(NULL , " ");
+            char *age_str= strtok(NULL , " ");
+
+	    if(!id_str || !name || !age_str){
+	    	printf("usage :add <id> <name><age>\n");
+		continue;
+	    }
+	    int id= atoi(id_str);
+	    int age=atoi(age_str);
+
 
             addUserTodb(id , name , age);
         }
         
+	if(strcmp(token, "list")==0){
+		printf("inside list ");
+		ListAllUsers();
+	}
     }
     
     return 0;
